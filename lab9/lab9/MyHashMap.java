fe12e7285b194a50e4631bcb0a40265849ed33ce
Shaynesmq <31604621+Shaynesmq@users.noreply.github.com>
2019-07-19 22:57:49 +0800

    Add files via upload

---
+++ lab9/lab9/MyHashMap.java
@@ -0,0 +1,141 @@
+package lab9;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.HashSet;
+
+/**
+ *  A hash table-backed Map implementation. Provides amortized constant time
+ *  access to elements via get(), remove(), and put() in the best case.
+ *
+ *  @author Mengqi Shen
+ */
+public class MyHashMap<K, V> implements Map61B<K, V> {
+
+    private static final int DEFAULT_SIZE = 16;
+    private static final double MAX_LF = 0.75;
+
+    private ArrayMap<K, V>[] buckets;
+    private int size;
+
+    private int loadFactor() {
+        return size / buckets.length;
+    }
+
+    public MyHashMap() {
+        buckets = new ArrayMap[DEFAULT_SIZE];
+        this.clear();
+    }
+
+    /* Removes all of the mappings from this map. */
+    @Override
+    public void clear() {
+        this.size = 0;
+        for (int i = 0; i < this.buckets.length; i += 1) {
+            this.buckets[i] = new ArrayMap<>();
+        }
+    }
+
+    /** Computes the hash function of the given key. Consists of
+     *  computing the hashcode, followed by modding by the number of buckets.
+     *  To handle negative numbers properly, uses floorMod instead of %.
+     */
+    private int hash(K key) {
+        if (key == null) {
+            return 0;
+        }
+
+        int numBuckets = buckets.length;
+        return Math.floorMod(key.hashCode(), numBuckets);
+    }
+
+    /* Returns the value to which the specified key is mapped, or null if this
+     * map contains no mapping for the key.
+     */
+    @Override
+    public V get(K key) {
+        ArrayMap<K, V> bucket = buckets[hash(key)];
+        return bucket.get(key);
+    }
+
+    /* Associates the specified value with the specified key in this map. */
+    @Override
+    public void put(K key, V value) {
+        if (get(key) != null) {
+            buckets[hash(key)].put(key, value);
+            return;
+        }
+
+        if (loadFactor() >= MAX_LF) {
+            resize(buckets.length * 2);
+        }
+
+        buckets[hash(key)].put(key, value);
+        size = size + 1;
+    }
+
+    private void resize(int capacity) {
+        ArrayMap<K, V>[] oldBuckets = buckets;
+        buckets = new ArrayMap[capacity];
+        for (int i = 0; i < buckets.length; i += 1) {
+            buckets[i] = new ArrayMap<>();
+        }
+
+        for (ArrayMap<K, V> m : oldBuckets) {
+            for (K k : m) {
+                buckets[hash(k)].put(k, m.get(k));
+            }
+        }
+    }
+
+    /* Returns the number of key-value mappings in this map. */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
+
+    /* Returns a Set view of the keys contained in this map. */
+    @Override
+    public Set<K> keySet() {
+        Set<K> ks = new HashSet<>();
+        for (ArrayMap<K, V> m : buckets) {
+            for (K k : m) {
+                ks.add(k);
+            }
+        }
+        return ks;
+    }
+
+    /* Removes the mapping for the specified key from this map if exists.
+     * Not required for this lab. If you don't implement this, throw an
+     * UnsupportedOperationException. */
+    @Override
+    public V remove(K key) {
+        if (get(key) == null) {
+            return null;
+        }
+
+        size = size - 1;
+        return buckets[hash(key)].remove(key);
+    }
+
+    /* Removes the entry for the specified key only if it is currently mapped to
+     * the specified value. Not required for this lab. If you don't implement this,
+     * throw an UnsupportedOperationException.*/
+    @Override
+    public V remove(K key, V value) {
+        if (get(key) == null || get(key) != value) {
+            return null;
+        }
+
+        size = size - 1;
+        return buckets[hash(key)].remove(key);
+    }
+
+    @Override
+    public Iterator<K> iterator() {
+        return keySet().iterator();
+    }
+}
