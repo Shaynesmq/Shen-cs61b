15f81410ef96d8fb712ae743eeddbc999b91743e
Shaynesmq <31604621+Shaynesmq@users.noreply.github.com>
2019-08-08 02:13:59 +0800

    Add files via upload

--- lab12/MergeSort.java
+++ lab12/MergeSort.java
@@ -69,6 +69,10 @@ public class MergeSort {
     /** Returns a Queue that contains the given items sorted from least to greatest. */
     public static <Item extends Comparable> Queue<Item> mergeSort(
             Queue<Item> items) {
+        if (items.size() <= 1) {
+            return items;
+        }
+
         Queue<Queue<Item>> siq = makeSingleItemQueues(items);
 
         while (siq.size() > 1) {
